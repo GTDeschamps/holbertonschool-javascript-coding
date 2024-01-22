89371a98fe97822b6c1869f050dbca60e99443a4
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-22 15:08:45 +0100

    Task 2: script to display status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//write a script to display the status code
+
+const request = require ('request')
+const url = process.argv[2];
+
+request.get(url, (error,response,body) => {
+  if (error) {
+    console.error(`code: ${error.message}`);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+    }
+});
