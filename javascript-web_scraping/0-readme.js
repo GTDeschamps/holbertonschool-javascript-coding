d1bdae20b383b663815e91825d48d247b0d20cd1
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-22 13:51:18 +0100

    Add code for Task 0: read file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//write a script to read and print the content of a file.
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err,filecontent) => {
+	if (err) {
+		console.error(err);
+	}
+	else {
+		console.log(filecontent)
+	}
+});
