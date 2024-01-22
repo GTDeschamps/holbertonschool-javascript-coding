3de4055ac0a49bfb74c364dbb5951130ba7138d2
GTDeschamps <g.deschamps@hotmail.fr>
2024-01-22 14:05:06 +0100

    complete the code of Task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//Write a script that write a string in the file
+
+const fs = require('fs')
+const filePath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filePath, string, 'utf-8', (err) => {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+});
+
