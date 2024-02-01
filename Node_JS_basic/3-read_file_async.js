057f03771cd86cd271b3111ffae23921b767a4cc
GTDeschamps <g.deschamps@hotmail.fr>
2024-02-01 14:47:38 +0100

    Correction Task3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -5,28 +5,43 @@ function countStudents(path) {
     fs.readFile(path, 'utf8', (error, data) => {
       if (error) {
         reject(new Error('Cannot load the database'));
-      } else {
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-
-        if (lines.length <= 1) {
-          reject(new Error('No valid students in the database'));
-        }
-        const headers = lines[0].split(',');
-        const studentsData = lines.slice(1);
-
-        const Studentscount = studentsData.length;
-
-        console.log(`Number of students: ${Studentscount}`);
-
-        headers.forEach((field, index) => {
-          const fieldData = studentsData.map((student) => student.split(',')[index]);
-          const uniqueFieldData = [...new Set(fieldData.filter(Boolean))];
-          const fieldCount = uniqueFieldData.length;
-
-          console.log(`Number of students in ${field}: ${fieldCount}.List: ${uniqueFieldData.join(',')}`);
-        });
-        resolve(Studentscount);
+        return;
       }
+      const lines = data.trim().split('\n').slice(1);
+      let nbOfLine = 0;
+      let csStudent = 0;
+      let sweStudent = 0;
+      let csString = '';
+      let sweString = '';
+
+      for (const line of lines) {
+        if (line !== '') {
+          nbOfLine += 1;
+          const cutline = line.split(',');
+          if (cutline[3] === 'CS') {
+            csStudent += 1;
+            if (csString !== '') {
+              csString += ', ';
+            }
+            csString += cutline[0];
+          } else if (cutline[3] === 'SWE') {
+            sweStudent += 1;
+            if (sweString !== '') {
+              sweString += ', ';
+            }
+            sweString += cutline[0];
+          }
+        }
+      }
+      const result = {
+        sentence1: `Number of students: ${nbOfLine}`,
+        sentence2: `Number of students in CS: ${csStudent}. List: ${csString}`,
+        sentence3: `Number of students in SWE: ${sweStudent}. List: ${sweString}`,
+      };
+      console.log(result.sentence1);
+      console.log(result.sentence2);
+      console.log(result.sentence3);
+      resolve(result);
     });
   });
 }
