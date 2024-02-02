fb88eda69f3c89bc718f03385ee6805d944a4cd0
GTDeschamps <g.deschamps@hotmail.fr>
2024-02-02 16:05:07 +0100

    correction Task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'Text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 const port = 1245;
@@ -10,6 +10,6 @@ module.exports = app;
 
 if (require.main === module) {
   app.listen(port, () => {
-    console.log(`Server running at http://localhost: ${port}/`);
+    console.log(`Server is running at http://localhost: ${port}`);
   });
 }
